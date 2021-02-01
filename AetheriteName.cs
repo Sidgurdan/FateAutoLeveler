620058d2324bf51db62e55394b31d5f9925f37bf
Sidgurdun <archon.x@gmx.net>
2021-02-02 00:19:55 +0100

    - Fixes #1 Endless loop at 74 - Fixes #2 Missing Zones for 50-60 - Fixes #3 Missing Zones for 60-70 - Fixes #4 Missing Zones for 75-80

--- AetheriteName.cs
+++ AetheriteName.cs
@@ -5,26 +5,57 @@ namespace FateAutoLeveler
     public static class AetheriteName
     {
         public const string None = "None";
+        
         public const string HawthorneHut = "The Hawthorne Hut";
         public const string Quarrymill = "Quarrymill";
         public const string CampDrybone = "Camp Drybone";
         public const string Horizon = "Horizon";
         public const string CampBronzeLake = "Camp Bronze Lake";
+
+        // < 10
         public const string SummerfordFarms = "Summerford Farms";
+        // < 20
         public const string Aleport = "Aleport";
+        // < 35
         public const string CostaDelSol = "Costa del Sol";
+        // < 47
         public const string CampDragonhead = "Camp Dragonhead";
         public const string CampOverlook = "Camp Overlook";
         public const string FalconsNest = "Falcon's Nest";
+        // < 53
+        public const string CoerthasWesternHighlands = "Coerthas Western Highlands";
+        // < 57
+        public const string ChurningMists = "Churning Mists";
+        // < 60
+        public const string DravanianHinterlands = "Dravanian Hinterlands";
+
+        // < 63
         public const string ThePeaksNorth = "Ala Gannha";
+        // < 65
         public const string TheRubySea = "Tamamizu";
+
+        // < 66
         public const string Yanxia = "Namai";
+
+        // < 68
         public const string TheAximSteppe = "Reunion";
+        
+        // < 70
         public const string TheLochs = "Porta Praetoria";
+        
+        // < 73
         public const string Stilltide = "Stilltide";
         public const string FortJobb = "Fort Jobb";
+        // < 74
         public const string LydhaLran = "Lydha Lran";
         public const string Wolekdorf = "Wolekdorf";
+        // < 75
+        public const string Slitherbough = "Slitherbough";
+        // < 78
+        public const string Twine = "Twine";
+        // < 80
+        public const string Tomra = "Tomra";
+        // End 70-80
 
         // TODO
         public static Dictionary<uint, string> GetTargetAetherites()
