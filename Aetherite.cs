3f4f65f606a54eac3c5f94de42bf41448c80a084
Sidgurdan <archon.x@gmx.net>
2021-02-17 08:38:31 +0100

    Add Feature: Allow multiple teleport targets per map for different level ranges - Group teleports by expansion - Allow execution with LizExFateBot and ExFateBot - Disable plugin until level up in case of missing teleport or other issue

---
+++ Aetherite.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ff14bot.Managers;
+
+namespace FateAutoLeveler
+{
+    public class Aetherite
+    {
+        public UInt32 ZoneId;
+        public string Name;
+        public uint MinLevel;
+        public uint MaxLevel;
+
+        public Aetherite(UInt32 locationZoneId, string locationName, uint min, uint max)
+        {
+            ZoneId = locationZoneId;
+            Name = locationName;
+            MinLevel = min;
+            MaxLevel = max;
+        }
+    }
+}
--- AetheriteName.cs
+++ AetheriteName.cs
@@ -5,62 +5,71 @@ namespace FateAutoLeveler
     public static class AetheriteName
     {
         public const string None = "None";
-        
+
+        #region 2.0 Zones
         public const string HawthorneHut = "The Hawthorne Hut";
         public const string Quarrymill = "Quarrymill";
         public const string CampDrybone = "Camp Drybone";
         public const string Horizon = "Horizon";
         public const string CampBronzeLake = "Camp Bronze Lake";
-
-        // < 10
         public const string SummerfordFarms = "Summerford Farms";
-        // < 20
         public const string Aleport = "Aleport";
-        // < 35
         public const string CostaDelSol = "Costa del Sol";
-        // < 47
         public const string CampDragonhead = "Camp Dragonhead";
         public const string CampOverlook = "Camp Overlook";
         public const string FalconsNest = "Falcon's Nest";
-        // < 53
+        #endregion
+
+        #region Heavensward 50-60
         public const string CoerthasWesternHighlands = "Coerthas Western Highlands";
-        // < 57
         public const string ChurningMists = "Churning Mists";
-        // < 60
-        public const string DravanianHinterlands = "Dravanian Hinterlands";
+        public const string Tailfeather = "Tailfeather";
+        #endregion
 
-        // < 63
+        #region Stormblood 60-70
         public const string ThePeaksNorth = "Ala Gannha";
-        // < 65
         public const string TheRubySea = "Tamamizu";
-
-        // < 66
         public const string Yanxia = "Namai";
-
-        // < 68
         public const string TheAximSteppe = "Reunion";
-        
-        // < 70
         public const string TheLochs = "Porta Praetoria";
-        
-        // < 73
+        #endregion
+
+        #region Shadowbringers 70-80
         public const string Stilltide = "Stilltide";
         public const string FortJobb = "Fort Jobb";
-        // < 74
         public const string LydhaLran = "Lydha Lran";
-        public const string Wolekdorf = "Wolekdorf";
-        // < 75
         public const string Slitherbough = "Slitherbough";
-        // < 78
         public const string Twine = "Twine";
-        // < 80
         public const string Tomra = "Tomra";
-        // End 70-80
+        #endregion
 
-        // TODO
-        public static Dictionary<uint, string> GetTargetAetherites()
+        public static Dictionary<uint, Aetherite> GetTargetAetherites()
         {
-            return new Dictionary<uint, string>();
+            Dictionary<uint, Aetherite> aetheriteList = new Dictionary<uint, Aetherite>();
+
+            uint i = 0;
+            aetheriteList.Add(++i, new Aetherite(134, SummerfordFarms, 1, 9));
+            aetheriteList.Add(++i, new Aetherite(138, Aleport, 10, 19));
+            aetheriteList.Add(++i, new Aetherite(145, CampDrybone, 20, 24));
+            aetheriteList.Add(++i, new Aetherite(153, Quarrymill, 25, 29));
+            aetheriteList.Add(++i, new Aetherite(137, CostaDelSol, 30, 34));
+            aetheriteList.Add(++i, new Aetherite(155, CampDragonhead, 35, 46));
+            aetheriteList.Add(++i, new Aetherite(180, CampOverlook, 47, 49));
+            aetheriteList.Add(++i, new Aetherite(397, CoerthasWesternHighlands, 50, 52));
+            aetheriteList.Add(++i, new Aetherite(400, ChurningMists, 53, 56));
+            aetheriteList.Add(++i, new Aetherite(398, Tailfeather, 57, 59));
+            aetheriteList.Add(++i, new Aetherite(620, ThePeaksNorth, 60, 62));
+            aetheriteList.Add(++i, new Aetherite(613, TheRubySea, 63, 64));
+            aetheriteList.Add(++i, new Aetherite(614, Yanxia, 65, 65));
+            aetheriteList.Add(++i, new Aetherite(622, TheAximSteppe, 66, 67));
+            aetheriteList.Add(++i, new Aetherite(621, TheLochs, 68, 69));
+            aetheriteList.Add(++i, new Aetherite(814, Stilltide, 70, 72));
+            aetheriteList.Add(++i, new Aetherite(816, LydhaLran, 73, 73));
+            aetheriteList.Add(++i, new Aetherite(817, Slitherbough, 74, 75));
+            aetheriteList.Add(++i, new Aetherite(815, Twine, 76, 77));
+            aetheriteList.Add(++i, new Aetherite(814, Tomra, 78, 80));
+
+            return aetheriteList;
         }
     }
 }
